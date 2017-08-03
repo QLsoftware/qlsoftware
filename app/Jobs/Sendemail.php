b1fb422eaa8db6615c70574139a5f1b1aaf9d3dd
QFP_ZJT <851207685@qq.com>
2017-08-03 11:55:55 +0800

    邮件修正_4

--- app/Jobs/Sendemail.php
+++ app/Jobs/Sendemail.php
@@ -49,21 +49,21 @@ class Sendemail extends Job implements ShouldQueue
     public function handle()
     {
         echo '邮件发送任务：';
-        try {
-            $flag = Mail::send('email.notification', ['name' => $this->name, 'link' => $this->link, 'from' => $this->from, 'data' => $this->data, 'title' => $this->title],
-                function ($message) {
-                    $message->to($this->email)->subject('智慧山大通知邮件');
-                });
-            if ($flag) {
-                Log::info($this->email . '发送成功');
-                echo '成功';
-            } else {
-                Log::info($this->email . '发送失败失败');
-                echo '失败';
-            }
-        } catch (\Exception $exception) {
+//        try {
+        $flag = Mail::send('email.notification', ['name' => $this->name, 'link' => $this->link, 'from' => $this->from, 'data' => $this->data, 'title' => $this->title],
+            function ($message) {
+                $message->to($this->email)->subject('智慧山大通知邮件');
+            });
+        if ($flag) {
+            Log::info($this->email . '发送成功');
+            echo '成功';
+        } else {
             Log::info($this->email . '发送失败失败');
+            echo '失败';
         }
+//        } catch (\Exception $exception) {
+//            Log::info($this->email . '发送失败失败');
+//        }
         sleep(10);
     }
 }
