40761bb9e80b5cfc40bfda2e0628b5b7dbd2b3c6
QFP_ZJT <851207685@qq.com>
2017-08-03 20:15:37 +0800

    welcome界面更新

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
+    <div class="container">
+        <div class="row">
 
-
+            <h1>Welcome</h1>
+        </div>
     </div>
-</div>
 @endsection
