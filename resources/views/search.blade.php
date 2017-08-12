29dbd0ae01d2d0e92f3f684f64abd5deeaacc4ce
apple <apple@appledeMacBook-Air.local>
2017-08-12 20:06:13 +0800

    qzk first

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,7 +2,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    <!-- dsfs-->
+    <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
