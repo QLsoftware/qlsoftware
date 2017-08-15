0477083f8c57a790dd3e1cf7c72d0f9d665d05ef
QFP_ZJT <851207685@qq.com>
2017-08-16 07:42:33 +0800

    footer

--- vendor/encore/laravel-admin/views/partials/footer.blade.php
+++ vendor/encore/laravel-admin/views/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Powered by <a href="https://github.com/QLsoftware/qlsoftware" target="_blank">智慧山大，后台支持</a></strong>
 </footer>
