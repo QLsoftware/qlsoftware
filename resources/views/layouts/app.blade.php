87ef78968fef52e4b87bdae50a228731a72641c3
root <851207685@qq.com>
2017-08-30 22:11:10 +0800

    模板移动端测试

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,8 +27,44 @@
     <![endif]-->
     @yield('css')
 </head>
-<body>
 
+<body>
+{{--目录--}}
+<div id="templatemo_mobile_menu">
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="#templatemo_banner_slide"><i class="glyphicon glyphicon-home"></i> &nbsp; Home</a></li>
+        <li><a href="#templatemo_about"><i class="glyphicon glyphicon-briefcase"></i> &nbsp; About</a></li>
+        <li><a href="#templatemo_events"><i class="glyphicon glyphicon-bullhorn"></i> &nbsp; Events</a></li>
+        <li><a href="#templatemo_timeline"><i class="glyphicon glyphicon-calendar"></i> &nbsp; Timeline</a></li>
+        <li><a href="#templatemo_contact"><i class="glyphicon glyphicon-phone-alt"></i> &nbsp; Contact</a></li>
+    </ul>
+</div>
+
+{{--也是目录--}}
+<div class="container_wapper">
+    <div id="templatemo_banner_menu">
+        <div class="container-fluid">
+            <div class="col-xs-4 templatemo_logo">
+                <a rel="nofollow" href="http://www.cssmoban.com/preview/templatemo_411_dragonfruit">
+                    <img src="images/templatemo_logo.jpg" id="logo_img" alt="dragonfruit free html5 template" />
+                    <h1 id="logo_text">dragon<span>fruit</span></h1>
+                </a>
+            </div>
+            <div class="col-sm-8 hidden-xs">
+                <ul class="nav nav-justified">
+                    <li><a href="#templatemo_banner_slide">Home</a></li>
+                    <li><a href="#templatemo_about">About</a></li>
+                    <li><a href="#templatemo_events">Events</a></li>
+                    <li><a href="#templatemo_timeline">Timeline</a></li>
+                    <li><a href="#templatemo_contact">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-8 visible-xs">
+                <a href="#" id="mobile_menu"><span class="glyphicon glyphicon-th-list"></span></a>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
