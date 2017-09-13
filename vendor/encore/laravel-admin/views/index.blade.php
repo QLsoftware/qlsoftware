191add80a47282e30f01b86b0fdea28ebe1434bd
root <851207685@qq.com>
2017-09-13 08:20:44 +0800

    fix qy route

--- vendor/encore/laravel-admin/views/index.blade.php
+++ vendor/encore/laravel-admin/views/index.blade.php
@@ -33,7 +33,6 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     {{--植入谷歌分析--}}
-    {{--{!! Analytics::render() !!}--}}
 </head>
 
 <body class="hold-transition {{config('admin.skin')}} {{join(' ', config('admin.layout'))}}">
