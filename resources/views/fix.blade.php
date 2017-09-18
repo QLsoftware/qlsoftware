263aa3513d62fe4e19768b696e7c329230131132
root <851207685@qq.com>
2017-09-18 23:28:50 +0800

    general

--- resources/views/fix.blade.php
+++ resources/views/fix.blade.php
@@ -181,7 +181,7 @@
                                                 @php($details = 'details'.$i)    <!-- 这个用于评价-->
                                                     @php($details_ = '#details'.$i)
                                                     {{--@php($id = '#id'.$i)--}}
-                                                    {{--@if(Auth::user()->j_username==$data->re_xh)--}}
+                                                    @if(true)
                                                         <tr>
                                                             <td>{{$data->re_id}}</td>
                                                             <td>{{$data->re_date}}</td>
@@ -310,7 +310,7 @@
                                                                         </div>
                                                                     </div>
 
-                                                    {{--@endif--}}
+                                                    @endif
 
                                                 </form>
 
