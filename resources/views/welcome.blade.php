4af45f5b31d520089c0a2fb0d44eae0a348612d8
root <851207685@qq.com>
2017-09-19 00:08:47 +0800

    general

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,7 @@
                 <!-- Start Carousel Section -->
                 <div class="home-slider">
                     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"
-                         style="padding-bottom: 30px;">
+                         {{--style="padding-bottom: 30px;"--}}>
                         <!-- Indicators -->
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -113,7 +113,7 @@
 
                         <!-- 滚动图片 -->
                         <div class="carousel-inner">
-                            <div class=" active item menu-item ">
+                            <div class=" active item menu-item light-orange">
                                 {{--<a href="{{ url('/forums') }}" data-toggle="modal">--}}
                                 @foreach($a1 as $a)
                                     <a href={{$a->href}}> {{$a->title.'  '.$a->data}}</a>
