ec8c8439387576deb2d9c17a49738321496c4949
root <851207685@qq.com>
2017-09-18 23:15:07 +0800

    general

--- app/Http/Controllers/FixController.php
+++ app/Http/Controllers/FixController.php
@@ -36,8 +36,8 @@ class FixController extends Controller
 
     public function fix_history()
     {   $SearchOption=['SearchOption' => 1];$repairData = repair::paginate(1);
-     /*   $xuehao = Auth::user()->j_username;
-        $repairData = repair::where('re_xh',$xuehao)->get();*/
+        $xuehao = Auth::user()->j_username;
+        $repairData = repair::where(['re_xh'=>$xuehao])->get();
         return view("fix",['repairData'=>$repairData])->with($SearchOption);
     }
 
